06d41a3188cf27c2cd9eb9ffb6532cbd9e4ce74f
Keshee Miles <keshee.miles18@gmail.com>
2021-10-24 09:38:02 -0500

    all good over here message

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/status", (request, response) => {
-  response.send(JSON.stringify({ message: "Service healthy" }));
+  response.send(JSON.stringify({ message: "All good over here" }));
 });
 
 app.listen(4040, () => console.log("Listening on port 4040"));
