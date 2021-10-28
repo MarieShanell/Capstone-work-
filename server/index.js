1ee90632b92800f4d91351ada49917938cb9296f
Keshee Miles <keshee.miles18@gmail.com>
2021-10-27 21:38:42 -0500

    backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require('mongoose')
+const morgan = require("morgan")
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+)
+
+
+mongoose.connect(DB).then(()=> console.log("my database connection was successful"))
+const fruitRouter = require("./routes/fruitRoutes")
 const app = express();
 
-app.get("/status", (request, response) => {
-  response.send(JSON.stringify({ message: "All good over here" }));
-});
+app.use(morgan("dev"))
+app.use(express.json())
+
+
+app.use("/api/v1/fruits", fruitRouter)
 
 app.listen(4040, () => console.log("Listening on port 4040"));
