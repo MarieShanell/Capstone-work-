4e116c8016d9a790946516577f6ba7610ed79f2e
Keshee Miles <keshee.miles18@gmail.com>
2021-11-10 21:35:29 -0600

    text box entry

--- index.js
+++ index.js
@@ -10,7 +10,45 @@ dotenv.config();
 // declaring router
 const router = new Navigo(window.location.origin);
 
+router.hooks({
+  before: (done, params) => {
+    const page =
+      params && params.hasOwnProperty("page")
+        ? capitalize(params.page)
+        : "Home";
 
+    switch (page) {
+      case "Home":
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`
+          )
+          .then(response => {
+            state.Home.weather = {};
+            // console.log(response, state.Home.weather);
+            state.Home.weather.city = response.data.name;
+            state.Home.weather.temp = response.data.main.temp;
+            state.Home.weather.feelsLike = response.data.main.feels_like;
+            state.Home.weather.humidity = response.data.main.humidity;
+            state.Home.weather.description =
+              response.data.weather[0]["description"];
+            done();
+          })
+          .catch(err => console.log(err));
+        break;
+
+      default:
+        done();
+    }
+  }
+});
+
+router
+  .on({
+    "/": () => render(state.Home),
+    ":page": params => render(state[capitalize(params.page)])
+  })
+  .resolve();
 //3. render function
 function render(st = state.Home) {
   document.querySelector("#root").innerHTML = `
@@ -33,6 +71,18 @@ function addEventListener(st) {
       render(state[event.target.title]);
     })
   );
+  switch (st.page){
+    case "Selfcare":
+      const inputVaule = document.querySelector("#textboxid")
+      const submitbutton = document.querySelector("#submit")
+      const ptag = document.querySelector("#val")
+      submitbutton.addEventListener("click",event =>{
+        event.preventDefault()
+        const v = inputVaule.value
+        ptag.innerHTML = v
+      })
+    break;
+  }
 }
 
 // document
@@ -45,33 +95,7 @@ function addEventListener(st) {
 //   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
 // });
 
-router.hooks({
-  before: (done, params) => {
-    const page = params && params.hasOwnProperty("page") ? capitalize(params.page) : "Home";
 
-    if (page === "Home") {
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`
-        )
-        .then(response => {
-          state.Home.weather = {};
-          state.Home.weather.city = response.data.name;
-          state.Home.weather.temp = response.data.main.temp;
-          state.Home.weather.feelsLike = response.data.main.feels_like;
-          state.Home.weather.description = response.data.weather[0].main;
-          done();
-        })
-        .catch(err => console.log(err));
-    }
-  }
-});
 
-router
-  .on({
-    "/": () => render(state.Home),
-    ":page": params => render(state[capitalize(params.page)])
-  })
-  .resolve();
 
 
