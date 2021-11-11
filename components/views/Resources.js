91f84d8ce9b2f0f808d8acb607cca7047a910502
Keshee Miles <keshee.miles18@gmail.com>
2021-11-11 14:37:34 -0600

    added resource link

--- components/views/Resources.js
+++ components/views/Resources.js
@@ -4,6 +4,13 @@ export default () => html`
   <section id="Resources">
     <h3>RESOURCES</h3>
 
+    <ul>
+    <a href="https://uhchealthierlives.com/#:~:text=Healthier%20Lives%20Your%20Connection%20to%20Community%20Resources%20UnitedHealthcare,with%20medical%20care%2C%20food%2C%20housing%2C%20transportation%2C%20and%20more."
+    target="_blank"
+        rel="noopener noreferrer">
+    Healthier Lives Community Resources  </a>
+</ul>
+
 
 
       <a href="https://get.betterup.com/for-individuals/google-specialtycoaching-f?utm_source=google&utm_medium=cpc&utm_term=nutrition%20counseling&utm_content=124813564458&utm_campaign=BetterUp_Search_NonBrand_Motivations&device=c&creative=527309748448&utm_campaignid=13390534764&gclid=CjwKCAiA1aiMBhAUEiwACw25MQNnV5XGlo_IpycbuDMLqwzRGp7FXrjs-1wTMZxqAWlgNrJRv469mBoCvYEQAvD_BwE"
